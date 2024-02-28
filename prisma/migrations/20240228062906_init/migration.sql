5c3eef21ccf0e43d64611c146b948762a4a71db2
MARGAM VINAY <margamvinay77@gmail.com>
2024-02-28 12:00:15 +0530

    test

---
+++ prisma/migrations/20240228062906_init/migration.sql
@@ -0,0 +1,119 @@
+-- CreateTable
+CREATE TABLE `Admin` (
+    `username` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `Admin_username_key`(`username`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Student` (
+    `id` VARCHAR(191) NOT NULL,
+    `fullName` VARCHAR(191) NOT NULL,
+    `rollNo` INTEGER NULL,
+    `email` VARCHAR(191) NULL,
+    `mobile` VARCHAR(191) NULL,
+    `gender` VARCHAR(191) NULL,
+    `year` VARCHAR(191) NOT NULL,
+    `joiningyear` VARCHAR(191) NULL,
+    `address` VARCHAR(191) NULL,
+    `image` LONGBLOB NULL,
+    `academicyear` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NULL,
+    `role` VARCHAR(191) NOT NULL DEFAULT 'student',
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Timetable` (
+    `id` VARCHAR(191) NOT NULL,
+    `year` VARCHAR(191) NOT NULL,
+    `academicyear` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Days` (
+    `id` VARCHAR(191) NOT NULL,
+    `day` VARCHAR(191) NOT NULL,
+    `timetableId` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Periods` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `daysId` VARCHAR(191) NOT NULL,
+    `time` VARCHAR(191) NOT NULL,
+    `subject` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Attendence` (
+    `id` VARCHAR(191) NOT NULL,
+    `studentId` VARCHAR(191) NOT NULL,
+    `date` DATETIME(3) NOT NULL,
+    `year` VARCHAR(191) NULL,
+    `academicyear` VARCHAR(191) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Subject` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `attendenceId` VARCHAR(191) NOT NULL,
+    `time` VARCHAR(191) NOT NULL,
+    `subject` VARCHAR(191) NOT NULL,
+    `present` BOOLEAN NOT NULL DEFAULT false,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Assessment` (
+    `id` VARCHAR(191) NOT NULL,
+    `studentId` VARCHAR(191) NOT NULL,
+    `year` VARCHAR(191) NULL,
+    `studentName` VARCHAR(191) NULL,
+    `name` VARCHAR(191) NULL,
+    `status` VARCHAR(191) NULL,
+    `academicyear` VARCHAR(191) NULL,
+    `assessment` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `AssessmentSubject` (
+    `id` VARCHAR(191) NOT NULL,
+    `assessmentId` VARCHAR(191) NOT NULL,
+    `subject` VARCHAR(191) NOT NULL,
+    `theoryMarks` INTEGER NULL,
+    `practicalMarks` INTEGER NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Days` ADD CONSTRAINT `Days_timetableId_fkey` FOREIGN KEY (`timetableId`) REFERENCES `Timetable`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Periods` ADD CONSTRAINT `Periods_daysId_fkey` FOREIGN KEY (`daysId`) REFERENCES `Days`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Attendence` ADD CONSTRAINT `Attendence_studentId_fkey` FOREIGN KEY (`studentId`) REFERENCES `Student`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Subject` ADD CONSTRAINT `Subject_attendenceId_fkey` FOREIGN KEY (`attendenceId`) REFERENCES `Attendence`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Assessment` ADD CONSTRAINT `Assessment_studentId_fkey` FOREIGN KEY (`studentId`) REFERENCES `Student`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `AssessmentSubject` ADD CONSTRAINT `AssessmentSubject_assessmentId_fkey` FOREIGN KEY (`assessmentId`) REFERENCES `Assessment`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
