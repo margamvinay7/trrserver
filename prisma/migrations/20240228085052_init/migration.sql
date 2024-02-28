19fece1ed31b9731f6d90408086ba1fd55a72ff1
MARGAM VINAY <margamvinay77@gmail.com>
2024-02-28 14:21:29 +0530

    new

--- prisma/migrations/20240228085052_init/migration.sql
+++ prisma/migrations/20240228085052_init/migration.sql
@@ -3,7 +3,8 @@ CREATE TABLE `Admin` (
     `username` VARCHAR(191) NOT NULL,
     `password` VARCHAR(191) NOT NULL,
 
-    UNIQUE INDEX `Admin_username_key`(`username`)
+    UNIQUE INDEX `Admin_username_key`(`username`),
+    PRIMARY KEY (`username`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
